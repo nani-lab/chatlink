0c7b9d24478738f027076dc3bec27535c53f6608
nani-lab <51260921+nani-lab@users.noreply.github.com>
2020-11-25 18:35:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 var socket = require("socket.io");
 
 
-var server = app.listen(3000, function () {
-    console.log("Listening on port 3000");
+var server = app.listen(8080, function () {
+    console.log("Listening on port 8080");
 });
 
 app.use(express.static("public"));
