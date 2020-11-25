0d52270ca4fc48fb04875319bfb8d13d8a7a30cc
nani-lab <51260921+nani-lab@users.noreply.github.com>
2020-11-25 19:39:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var server = app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 var io = socket(server);
 
