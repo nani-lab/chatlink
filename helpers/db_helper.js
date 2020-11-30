ed91a9333bde6c1813fddad09609842ba9ea91e5
nani-lab <51260921+nani-lab@users.noreply.github.com>
2020-12-01 04:28:17 +0530

    Add files via upload

---
+++ helpers/db_helper.js
@@ -0,0 +1,44 @@
+'use strict';
+const mysql = require('mysql');
+
+class Helper {
+
+    constructor() {
+
+        this.pool = mysql.createPool({
+            connectionLimit: 100,
+            /* host: '148.66.145.4',
+            user: 'qw_user',
+            password: 'Qwick@123',
+            database: 'qw_db', */
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'touch',
+
+            debug: false
+        });
+    }
+
+    saveMessage(message, senderId, senderType, receiverId, receiverType){
+        const currentDate = new Date();
+        /* if (err) {
+            callback({ "error": true });
+        } */
+        this.pool.getConnection((err, connection) => {
+            console.log("INSERT INTO chat_messages(messages,senderId,senderType,receiverId,receiverType,createdDate) VALUES('"+message+"','"+senderId+"','"+senderType+"','"+receiverId+"','"+receiverType+"','"+currentDate+"')");
+            connection.query("INSERT INTO chat_messages(messages,sender_id,sender_type,receiver_id,receiver_type,created_at) VALUES('"+message+"','"+senderId+"','"+senderType+"','"+receiverId+"','"+receiverType+"','"+currentDate+"')", (err, rows) => {
+                /* if (!err) {
+                    callback({"error":false,"rows":rows});
+                } */
+            });
+
+            connection.on('error',  (err) => {
+                callback({ "error": true });
+            });
+        });
+    }
+
+}
+
+module.exports = new Helper();
