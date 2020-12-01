34bd4f6f0ea0b944fabd40d21f0279e2fc28be9b
nani-lab <51260921+nani-lab@users.noreply.github.com>
2020-12-01 12:21:23 +0530

    Update db_helper.js

--- helpers/db_helper.js
+++ helpers/db_helper.js
@@ -5,6 +5,24 @@ class Helper {
 
     constructor() {
         
+        var con = mysql.createConnection({
+  
+            host: '148.66.145.4',
+            port: '3306',
+            user: 'qw_user',
+            password: 'Qwick@123',
+            database: 'qw_db',
+
+ 
+
+    });
+
+ 
+
+     con.connect(function(err) {
+        if (err)  console.log(err);
+        console.log("Connected!");
+     });
         this.pool = mysql.createPool({
             connectionLimit: 100,
             host: '148.66.145.4',
