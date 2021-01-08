6743b432def64c72546ab091496c8144e2168417
nani-lab <51260921+nani-lab@users.noreply.github.com>
2021-01-08 13:59:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,219 +1,25 @@
 const io = require('socket.io')(process.env.PORT || 3000);
 const helper = require('./helpers/db_helper');
-//stats
-let numUsers = 0;
-let idRoom = 2;
-let idUser = 1;
-
-let users = [];
-/*[{
-          "id" : 1
-          "username" : "michel",
-          "myRooms" : [1, 2, 3, 4]
-
-}]*/
-let rooms = [{
-    'id':1,
-    'name':'test',
-    'numUsers':0,
-    'users':[]
-}];
-/*[{
-    "id": 1,
-    "name": "hello",
-   "numUsers": 1,
-    "users" : [1, 2, 3, 4],
-}]*/
-let messages = {'1':[]};
-/*{
-    "1": [{
-         "message" : "hello",
-         "user_id" : 1, //if 0 is server send message type
-          }]
-}*/
+var moment = require('moment-timezone');
 var connectedUsers = {}; 
 
-
-
+// socket connection
 io.on('connection', socket => {
+    // if connected, emit to socket saying connected with the socket.id
     socket.emit('connected', socket.id);
-  //  console.log(JSON.stringify(socket));
-    // if(typeof socket.handshake.name!="undefined"){
-    //    var user = {
-    //       name : socket.handshake.query.name,
-    //       id : socket.handshake.query.id,
-    //       email : socket.handshake.query.email,
-    //     }
-    //     console.log(user);
-    //   }
+    /*--------------------------New User - Start---------------------------*/
+    // when user added, store the socket information in connectedUsers
     socket.on('add-user', userId => {
         if(userId) {
             socket.user_id = userId;
-            connectedUsers[userId] = socket;
-            console.log('Added: ' + socket.user_id );
+            // connectedUsers[userId] = socket;
+            connectedUsers[userId].push(socket);
+            // console.log('Added: ' + socket.user_id );
             // console.log(connectedUsers[userId]);
         }
     });
-    socket.on('add user', username => {
-        console.log('connection ' + username);
-
-        socket.user_id = idUser;
-        users.push({
-            id: idUser,
-            username: username,
-            myRooms: []
-        });
-        idUser++;
-        numUsers++;
-
-        io.sockets.emit('update users', users);//emit to all people
-        socket.emit('update rooms', rooms);//emit only for client
-        socket.emit('login', socket.user_id);
-    });
-
-    socket.on('add room', roomName => {
-        let room = {
-            id: idRoom,
-            name:roomName,
-            numUsers: 0,
-            users: []
-        };
-
-        rooms.push(room);
-        io.sockets.emit('update rooms', rooms);//emit to all people
-
-        messages[idRoom] = [];
-        idRoom++;
-
-        socket.emit('room created', room);//emit only to client
-    });
-
-    socket.on('connect room', room => {
-        socket.join(room.id);
-        let message = {
-            'user_id':0,
-            'message': users.find(x => x.id === socket.user_id).username + ' join the room ' + room.name
-        };
-        socket.broadcast.in(room.id).emit('new message', message);
-        messages[room.id].push(message);
-
-        rooms.find(x => x.id === room.id).users.push(socket.user_id);
-        rooms.find(x => x.id === room.id).numUsers++;
-        io.sockets.emit('update rooms', rooms);//emit to all people
-
-        users.find(x => x.id === socket.user_id).myRooms.push(room.id);
-        io.sockets.emit('update users', users);//emit to all people
-
-        socket.emit('go room',
-            {
-                room,
-                messages:messages[room.id]
-            });//emit only to client
-    });
-
-    socket.on('join room', room => {
-        let message = {
-            'user_id':0,
-            'message': users.find(x => x.id === socket.user_id).username + ' join the room ' + room.name
-        };
-        socket.broadcast.in(room.id).emit('new message', message);
-        messages[room.id].push(message);
-        rooms.find(x => x.id === room.id).numUsers++;
-
-        io.sockets.emit('update rooms', rooms);//emit to all people
-
-        socket.emit('go room',
-            {
-                room,
-                messages:messages[room.id]
-            });//emit only to client
-    });
-
-    socket.on('leave room', room => {
-        let message =  {
-            'user_id'   :   0,
-            'username'  :   'server',
-            'message'   :   users.find(x => x.id === socket.user_id).username + ' leave the room ' + room.name
-        };
-        socket.broadcast.in(room.id).emit('new message', message);
-        messages[room.id].push(message);
-        rooms.find(x => x.id === room.id).numUsers = (rooms.find(x => x.id === room.id).numUsers === 0) ? 0 : rooms.find(x => x.id === room.id).numUsers - 1;
-        io.sockets.emit('update rooms', rooms);//emit to all people
-        socket.emit('go leave room');
-    });
-
-    socket.on('quit room', room => {
-        socket.leave(room.id);
-        let message =  {
-            'user_id'   :   0,
-            'username'  :   'server',
-            'message'   :   users.find(x => x.id === socket.user_id).username + ' quit the room ' + room.name
-        };
-        socket.broadcast.in(room.id).emit('new message', message);
-
-        let myRooms = users.find(x => x.id === socket.user_id).myRooms;
-        myRooms = myRooms.filter(item => item !== room.id);
-        users.find(x => x.id === socket.user_id).myRooms = myRooms;
-        io.sockets.emit('update users', users);//emit to all people
-
-        let idUsers = rooms.find(x => x.id === room.id).users;
-        idUsers = idUsers.filter(item => item !== socket.user_id);
-        rooms.find(x => x.id === room.id).users = idUsers;
-        io.sockets.emit('update rooms', rooms);//emit to all people
-
-        socket.emit('go quit room');
-    });
-
-    // socket.on('send message', data => {
-    //     let message =  {
-    //         'user_id': socket.user_id,
-    //         'username': users.find(x => x.id === socket.user_id).username,
-    //         'message': data.message
-    //     };
-    //     messages[data.room.id].push(message);
-    //     socket.nsp.to(data.room.id).emit('new message', message);
-    // });
-
-    socket.on('send message', data => {
-        let message =  {
-            'user_id': data.user,
-            // 'username': users.find(x => x.id === socket.user_id).username,
-            'message': data.text
-        };
-        console.log(message);
-        // socket.nsp.to(data.user).emit('new message', message);
-        // socket.broadcast.in(data.user).emit('new message', message);
-        helper.saveMessage(message,data.user,data.senderType,data.receiverId,data.receiverType,(result)=>{
-            if (result.error) {
-                response.status(100).json({ "error": true,"message": "Error in connection database" });
-            }else if(result.rows.length === 0){
-                response.status(404).json({ "error": true,"message": "No result Found" });
-            }else{
-                response.status(200).json(result);
-            }
-        });
-
-        socket.emit('message', {msg: message.text, user: message.user, createdAt: new Date()});    
-    
-    });
-    
-    socket.on('send-message', (message) => {
-        console.log(message);
-        helper.saveMessage(message.text,message.senderId,message.senderType,message.receiverId,message.receiverType,(result)=>{
-            if (result.error) {
-                response.status(100).json({ "error": true,"message": "Error in connection database" });
-            }else if(result.rows.length === 0){
-                response.status(404).json({ "error": true,"message": "No result Found" });
-            }else{
-                response.status(200).json(result);
-            }
-        });
-        // connectedUsers['1'].emit('something', 'something');
-        //io.emit('message', {msg: message.text, user: message.user, createdAt: new Date()}); 
-        //socket.emit('message', {msg: message.text, senderId: message.senderId, receiverId: message.receiverId, createdAt: new Date()}); 
-
-    
-    });
+    /*--------------------------New User - End-----------------------------*/
+ 
     socket.on('send-message1', (message) => {
             console.log(message);
             helper.saveMessage(message.text,message.senderId,message.senderType,message.receiverId,message.receiverType,(result)=>{
@@ -226,12 +32,15 @@ io.on('connection', socket => {
             }
             });
                 
-     if(message.senderId!= undefined && message.receiverId != undefined && connectedUsers[message.receiverId] != undefined) {
-        console.log(connectedUsers[message.receiverId].user_id);
-   connectedUsers[message.receiverId].emit('message', {msg: message.text, senderId: message.senderId, receiverId: message.receiverId, createdAt: new Date()});
-     }
- // io.emit('message', {msg: message.text, senderId: message.senderId, receiverId: message.receiverId, createdAt: new Date()}); 
- });
+        if(message.senderId!= undefined && message.receiverId != undefined && connectedUsers[message.receiverId] != undefined) {
+            // console.log(connectedUsers[message.receiverId].user_id);
+            var cstTimeNow = moment().tz('America/Chicago').format('D MMM YYYY, h:mm a');
+            //connectedUsers[message.receiverId].emit('message', {msg: message.text, senderId: message.senderId, receiverId: message.receiverId, createdAt: cstTimeNow});
+            connectedUsers[message.receiverId].foreach(m=> {
+                m.emit('message', {msg: message.text, senderId: message.senderId, receiverId: message.receiverId, createdAt: cstTimeNow});
+            });
+        }
+    });
     socket.on('start typing', roomId => {
         socket.broadcast.to(roomId).emit('new typing', socket.user_id);
     });
@@ -252,7 +61,7 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
 
         console.log('deconnection');
-        numUsers--;
+        // numUsers--;
 
 
 
